91f2d632e54ceb1c0539282124b2293eeb687768
dhannypramana <p.dhannypramana@gmail.com>
2022-03-07 23:45:29 +0700

    update small things

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,8 +5,18 @@
 @endsection
 
 @section('main')
-    <h1>Ini Halaman About</h1>
-    <h3>{{ $user[0]->name }}</h3>
-    <h5>{{ $user[0]->email }}</h5>
-    <h5>{{ $user[0]->password }}</h5>
+    <table class="table table-borderless">
+        <tr>
+            <td><h5>Name</h5></td>
+            <td><h5>{{ $user->name }}</h5></td>
+        </tr>
+        <tr>
+            <td><h5>e-Mail</h5></td>
+            <td><h5>{{ $user->email }}</h5></td>
+        </tr>
+        <tr>
+            <td><h5>Key</h5></td>
+            <td><h5>{{ $user->password }}</h5></td>
+        </tr>
+    </table>
 @endsection
