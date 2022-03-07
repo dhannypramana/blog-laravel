81e0a9c3925866a0bb6de0cf2c60d97e76e35d90
dhannypramana <p.dhannypramana@gmail.com>
2022-03-08 01:06:34 +0700

    update user feature

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -6,7 +6,7 @@
     @foreach ($posts as $post)
         <article style="border: 1px solid black; padding: 20px;" class="mb-3">
             <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
-            <h5>Author : {{ $post->user->name }} in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
+            <h5>Author : <a href="/user/{{ $post->user->slug }}">{{ $post->user->name }}</a> in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
             <p>{!! $post->excerpt !!}</p>
             <a href="/posts/{{ $post->slug }}">Read More</a>
         </article>
